383cb66b12c097d4521fe81abf9de308e6ab6253
Ali <ali.bakhshi021.001@gmail.com>
2025-06-06 14:09:00 +0330

    api-Not found 404

--- product_module/api/v1/serializers.py
+++ product_module/api/v1/serializers.py
@@ -1,2 +1,5 @@
 from rest_framework import serializers
 
+class Product_serializers(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=250)
--- product_module/api/v1/views.py
+++ product_module/api/v1/views.py
@@ -1,6 +1,8 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from .serializers import Product_serializers
+from ...models import Product
 
 @api_view()
 @permission_classes([AllowAny])
@@ -10,4 +12,9 @@ def product_view(request):
 @api_view()
 @permission_classes([AllowAny])
 def detail_view(request, id):
-    return Response(id)
+    try:
+        product = Product.objects.get(pk=id)
+        serializers = Product_serializers(product)
+        return Response(serializers.data)
+    except:
+        return Response("{'detail is not found'}", status=404)
