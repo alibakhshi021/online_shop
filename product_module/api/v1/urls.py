95d9d8cf3a273a39ef221471d4c74d29fea64e74
Ali <ali.bakhshi021.001@gmail.com>
2025-06-11 09:25:08 +0330

    refactor: class (viewsets.ViewSet)

--- product_module/api/v1/urls.py
+++ product_module/api/v1/urls.py
@@ -4,7 +4,9 @@ from . import views
 app_name= 'api-v1'
 
 urlpatterns = [
-    path('post/', views.ProductList.as_view(), name='product_list_api'),
-    path('post/<int:pk>/', views.ProductDetail.as_view(), name='product_detail_api'),
+    # path('post/', views.ProductList.as_view(), name='product_list_api'),
+    # path('post/<int:pk>/', views.ProductDetail.as_view(), name='product_detail_api'),
+    path('post/', views.ProductListView.as_view({'get':'list'}), name='product_list'),
+    path('post/<int:pk>/', views.ProductListView.as_view({'get':'retrieve'}), name='product_detail'),
     
 ]
--- product_module/api/v1/views.py
+++ product_module/api/v1/views.py
@@ -8,6 +8,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.generics import GenericAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework import mixins
+from rest_framework import viewsets
 
 class ProductList(ListCreateAPIView):
     """getting a list of post and creating new posts"""
@@ -21,3 +22,20 @@ class ProductDetail(RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = ProductSerializers
     queryset = Product.objects.filter(is_active=True)
+
+
+
+class ProductListView(viewsets.ViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_classes = ProductSerializers
+    queryset = Product.objects.filter(is_active=True)
+
+    def list(self, request):
+        serializer = self.serializer_classes(self.queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        product_object = get_object_or_404(self.queryset, pk=pk)
+        serializer = self.serializer_classes(product_object)
+        return Response(serializer.data)
+        
