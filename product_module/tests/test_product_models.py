7d3fac0f83007f1939c30d64a9b1d1e0b71c250a
Ali <ali.bakhshi021.001@gmail.com>
2025-08-08 11:12:19 +0330

    Add: crate Test for view adn models : product-module

--- product_module/tests/test_product_models.py
+++ product_module/tests/test_product_models.py
@@ -59,5 +59,36 @@ class TestProductModel(TestCase):
         self.assertEqual(product.title, 'iphone 15 pro')
         self.assertEqual(product.get_in_is_active(), 'موجود میباشد')
         self.assertIn(self.cat1, product.category.all())
-        
-        
+
+class TestProductTagModel(TestCase):
+
+    def setUp(self):
+        self.category = ProductCategory.objects.create(
+            title = 'test',
+            url_title = 'test',
+            is_active = True,
+            is_delete = False,
+        )
+
+        self.brand = ProductBrand.objects.create(
+            title = 'LG',
+            url_title = 'LG',
+            is_active = True
+        )
+
+        self.product = Product.objects.create(
+            title = 'iphone 15 pro',
+            brand = self.brand,
+            stock_quantity = 2,
+            price = 15000000,
+            short_description = '2025',
+            description = '2025American',
+            is_active = True,
+            is_delete = False,
+        )
+
+        self.product.category.set([self.category])
+
+    def test_product_tag(self):
+        tag = ProductTag.objects.create(product=self.product, caption='2025')
+        self.assertEqual(tag.product, self.product)
